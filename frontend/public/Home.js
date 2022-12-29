4973bb1c4f59c9ebe165ad2773f2c251152284c9
jamesbrowne98 <james.browne@students.plymouth.ac.uk>
2022-12-29 14:22:47 +0000

    V1.11

--- frontend/public/Home.js
+++ frontend/public/Home.js
@@ -1,44 +1,8 @@
-$(function() {
-    $("#sayHello").click(function() {
-        let userName = $("#name").val();
+/* $(function() {
+    
+   }); */
+function trainingDirect() {
+   window.location ="Training.html";
+        }
 
-        let message = "Hello " + userName + "!";
-        $("#sayHello_result").html(message);
-    });
-   });
-
-   $(function() {
-    $("#checkNumber").click(function() {
-    let number = $("#number").val();
-    $("#numberMessage").removeClass("greater less");
-    if (number < 10) {
-    $("#numberMessageText").html("Less than ten");
-    $("#numberMessage").addClass("less");
-    } else {
-    $("#numberMessageText").html("Greater than or equal to ten");
-    $("#numberMessage").addClass("greater");
-    }
-    $("#numberMessage").removeClass("hidden");
-    });
-   });
    
-   function uniqueNumbers(numbers) {
-    let uniques = [];
-    for (i=0; i<numbers.length; i++) {
-    if (!uniques.includes(numbers[i])) {
-    uniques.push(numbers[i]);
-    }
-    }
-    return uniques;
-   }
-   $(function() {
-    $("#listUnique").click(function() {
-    let numbers = $("#numbers").val().split(",");
-    let vals = uniqueNumbers(numbers);
-    console.log("vals: " + vals);
-   
-    for (i=0; i<vals.length; i++) {
-    $("#uniqueNumbers").append(vals[i] + " ");
-    }
-    });
-   });
