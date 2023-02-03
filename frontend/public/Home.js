0ce1da1061e99ec4c325ae7862724e0a2e1516b3
jamesbrowne98 <72554634+jamesbrowne98@users.noreply.github.com>
2023-02-03 13:24:41 +0000

    1.00
    set up the html pages, reconfig folders structure

--- frontend/public/Home.js
+++
@@ -1,8 +0,0 @@
-/* $(function() {
-    
-   }); */
-function trainingDirect() {
-   window.location ="Training.html";
-        }
-
-   
---
+++ views/Home.js
@@ -0,0 +1,26 @@
+/* $(function() {
+    
+   }); */
+function trainingDirect() {
+   window.location ="Training.html";
+        }
+
+        function statsDirect() {
+         window.location ="stats.html";
+              }
+
+              function contactDirect() {
+               window.location ="contact.html";
+                    }
+
+                    function committeeDirect() {
+                     window.location ="committee.html";
+                          }
+
+                          function storeDirect() {
+                           window.location ="store.html";
+                                }
+
+                                function welcomeDirect() {
+                                 window.location ="Welcome.html";
+                                      }
