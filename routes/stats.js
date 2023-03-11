09f7ee1f58b1b410816f4e310b4d8cd40d491fb4
jamesbrowne98 <72554634+jamesbrowne98@users.noreply.github.com>
2023-03-11 18:42:34 +0000

    1.061
    Minor changes trying to fix bugs of connection

--- routes/stats.js
+++ routes/stats.js
@@ -1,39 +1,20 @@
-const mongoose = require('mongoose');
-const { ObjectId } = require('mongoose');
-
-const statsSchema = new mongoose.Schema({
-  // Define schema for stats data
-  _id: { type: ObjectId, required: true},
-  player_id: { type: String, required: true},
-  game_id: { type: String, required: true},
-  PlayerName: {type: String, required: true},
-  hits: {type: Number, required: true},
-  catches: {type: Number, required: true},
-  totalEliminations: {type: Number, required: true},
-  singleBallEliminations: {type: Number, required: true},
-  teamEliminations: {type: Number, required: true},
-  dodges: {type: Number, required: true},
-  timesHit: {type: Number, required: true},
-  singleOut: {type: Number, required: true},
-  teamOut: {type: Number, required: true},
-  miscOut: {type: Number, required: true},
-  timesCaught: {type: Number, required: true},
-  timesEliminated: {type: Number, required: true},
-  KD: {type: mongoose.Decimal128, required: true},
-  setsOff: {type: Number, required: true},
-});
-
 const express = require('express');
 const router = express.Router();
+
 const Stats = require('../models/stats');
 
-// GET all stats
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const stats = await Stats.find({ _id: id }); 
+  res.render('stats', { stats: stats });
+});
+
 router.get('/', async (req, res) => {
   try {
-    const stats = await Stats.find({}).limit(100).maxTimeMS(30000);
+    const stats = await Stats.find({});
     res.json(stats);
   } catch (error) {
-    console.error(error.message);
+    console.error(error);
     res.status(500).send('Server Error');
   }
 });
--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-
 // Use Morgan middleware to log requests
 app.use(morgan('dev'));
 
-const statsRouter = require('./routes/stats');
+// Serve static files from the views directory
 app.use(express.static('views'));
 
-
-app.use('/stats', (req, res) => {
+// Serve the stats.html file for the /stats route
+app.get('/stats', (req, res) => {
   res.sendFile(__dirname + '/views/stats.html');
 });
 
+// Mount the stats router at the /api/stats route
+const statsRouter = require('./routes/stats');
 app.use('/api/stats', statsRouter);
 
 app.listen(3000, () => {
