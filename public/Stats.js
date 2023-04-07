f89a9a34d4afcbfa8323115baa33eaed8b8afcee
jamesbrowne98 <james.browne@students.plymouth.ac.uk>
2023-04-07 11:40:43 +0100

    1.073
    Bug fixes, not fully

--- public/Stats.js
+++ public/Stats.js
@@ -41,106 +41,95 @@ fetch('/api/Stats')
   */
 
   // Fetch stats for the first game
-  import React, { useState, useEffect, useRef } from "react";
-  import * as d3 from "d3";
-  
-  function Stats() {
-    const [gameId, setGameId] = useState("");
-    const [playerIds, setPlayerIds] = useState([]);
-    const [stats, setStats] = useState([]);
-    const svgRef = useRef();
-  
-    useEffect(() => {
-      const fetchStats = async () => {
-        if (gameId !== "" && playerIds.length > 0) {
-          const response = await fetch(`/Stats/${gameId}`);
-          const data = await response.json();
-          const filteredStats = data.filter((stat) =>
-            playerIds.includes(stat.player_id)
-          );
-          setStats(filteredStats);
+import React, { useState, useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+function Stats() {
+  const [stats, setStats] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState("");
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    fetch("/api/Stats")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-      };
-      fetchStats();
-    }, [gameId, playerIds]);
+        return response.json();
+      })
+      .then((statsArray) => {
+        setStats(statsArray);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const handlePlayerChange = () => {
+    setSelectedPlayer(playerRef.current.value);
+  };
+
+  const filteredStats = stats.filter((s) => s.PlayerName === selectedPlayer);
+
+  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  const width = 960 - margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
+
+  const x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+  const y = d3.scaleLinear().rangeRound([height, 0]);
+
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+
+    if (filteredStats.length > 0) {
+      x.domain(filteredStats.map((d) => d.game_id));
+      y.domain([0, d3.max(filteredStats, (d) => d.KD.$numberDecimal)]);
+
+      svg
+        .select(".x-axis")
+        .call(d3.axisBottom(x))
+        .attr("transform", `translate(0, ${height})`);
+
+      svg.select(".y-axis").call(d3.axisLeft(y));
+
+      svg.selectAll(".bar").remove();
+
+      svg
+        .selectAll(".bar")
+        .data(filteredStats)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => x(d.game_id))
+        .attr("y", (d) => y(d.KD.$numberDecimal))
+        .attr("width", x.bandwidth())
+        .attr("height", (d) => height - y(d.KD.$numberDecimal));
+    } else {
+      svg.selectAll(".bar").remove();
+    }
+  }, [filteredStats]);
   
-    useEffect(() => {
-      if (stats.length > 0) {
-        const svg = d3.select(svgRef.current);
-        const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-        const width = +svg.attr("width") - margin.left - margin.right;
-        const height = +svg.attr("height") - margin.top - margin.bottom;
-        const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
-  
-        // Get an array of all the unique game IDs in the stats data
-        const gameIds = [...new Set(stats.map((stat) => stat.game_id))];
-  
-        // Create a scale for the x-axis using the unique game IDs
-        const x = d3.scaleBand().range([0, width]).domain(gameIds);
-  
-        // Create a scale for the y-axis using the maximum number of hits
-        const y = d3
-          .scaleLinear()
-          .range([height, 0])
-          .domain([0, d3.max(stats, (d) => d.hits)]);
-  
-        // Create a line generator function that uses the x and y scales
-        const line = d3
-          .line()
-          .x((d) => x(d.game_id))
-          .y((d) => y(d.hits));
-  
-        // Create an array of colors to use for each player's line
-        const colors = d3.scaleOrdinal(d3.schemeCategory10);
-  
-        // Group the stats data by player ID
-        const players = d3.group(stats, (d) => d.player_id);
-  
-        // Set the domain of the colors scale to the player IDs
-        colors.domain(players.keys());
-  
-        // Add the x-axis to the chart
-        g.append("g")
-          .attr("transform", `translate(0,${height})`)
-          .call(d3.axisBottom(x));
-  
-        // Add the y-axis to the chart
-        g.append("g").call(d3.axisLeft(y));
-  
-        // Loop through each player's stats and draw a line for each one
-        players.forEach((playerStats, playerId) => {
-          g.append("path")
-            .datum(playerStats)
-            .attr("fill", "none")
-            .attr("stroke", colors(playerId))
-            .attr("stroke-width", 1.5)
-            .attr("d", line);
-        });
-      }
-    }, [stats]);
-  
-    const handleGameIdChange = (event) => {
-      setGameId(event.target.value);
-    };
-  
-    const handlePlayerIdsChange = (event) => {
-      setPlayerIds(
-        Array.from(event.target.selectedOptions, (option) => option.value)
-      );
-    };
-  
-    return (
+  return (
+    <>
       <div>
-        <select multiple={true} value={playerIds} onChange={handlePlayerIdsChange}>
-          {players.map(player => <option key={player._id} value={player._id}>{player.name}</option>)}
+        <label htmlFor="playerSelect">Select a player:</label>
+        <select id="playerSelect" ref={playerRef} onChange={handlePlayerChange}>
+          <option value="">All players</option>
+          {stats.map((s) => (
+            <option key={s.player_id} value={s.PlayerName}>
+              {s.PlayerName}
+            </option>
+          ))}
         </select>
-        <input type="text" value={gameId} onChange={handleGameIdChange} />
-        <svg ref={svgRef} width={500} height={500}>
-        </svg>
       </div>
-    );
-     }
-  
-  export default Stats;
-  
-  
+      <svg ref={svgRef} width={width + margin.left + margin.right} height={height + margin.top + margin.bottom}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+    </>
+  );
+}
+
+export default Stats;
